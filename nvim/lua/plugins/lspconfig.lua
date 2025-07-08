6e166f23e34bc771e05a5d7c8f3d27702fd251f6
Kackspn <jpstreeter15@gmail.com>
2025-07-07 23:06:47 -0600

    add tiny inline diagnostics

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -108,5 +108,6 @@ return {
                 },
             },
         })
+        vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
     end,
 }
