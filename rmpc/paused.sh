3bc2f986b93989198598bcc2de6396102fd6dac4
Kackspn <jpstreeter15@gmail.com>
2025-08-11 15:51:29 -0600

    saoi

--- rmpc/paused.sh
+++ rmpc/paused.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PATH="/home/jackson/.cargo/bin:$PATH"
+rmpc togglepause
 
 PAUSED=$(rmpc status)
 DISPLAY=$(echo "$PAUSED" | jq -r '.state')
---
+++ waybar/waybarhypr/init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+killall waybar
+
+waybar -c ~/.config/waybar/waybarhypr/config & -s ~/.config/waybar/waybarhypr/style.css
+
