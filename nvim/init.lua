b07e94348803a68b84fe7d3579dc645a6589bb16
Kackspn <jpstreeter15@gmail.com>
2025-08-13 17:19:24 -0600

    ASD

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,7 +27,6 @@ vim.pack.add({
   { src = "https://github.com/scottmckendry/cyberdream.nvim" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
-vim.pack.update(vim.pack.get())
 
 -----------------------------------------------------
 --- PACKAGE SETUP
